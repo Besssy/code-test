a586b9fe29ef9dc18bb7cb7cb1fde8bc83262ad8
Bessy Theodoropoulou <mellonance@gmail.com>
2017-10-19 16:22:29 +0300

    Add mobile menu functionality and hide textarea placeholder.

--- client/js/main.js
+++ client/js/main.js
@@ -11,4 +11,23 @@ $(function(){
         var filename = m[1];
         $(this).parent('label').siblings('span').html(filename);
     });
+
+
+ 	$("#mynav").click(function(){
+ 		$(this).addClass('responsive');
+ 		$(".btn-close").show(); 
+        $("#content").animate({right: '260px'});  
+        $(".logo").hide();              
+ 	});
+ 	$(".btn-close").click(function(){
+ 		$("#mynav").removeClass('responsive');
+ 		$(this).hide();
+        $("#content").animate({right: '0px'}); 
+        $(".logo").show();  
+ 	});
+
+    $('.textarea').on('input', function(){
+        $(".placeholder").hide();
+    });
+   		
 });
